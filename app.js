78ea7688734d16696746addb589f709b7e427d77
Moahmed Wael <midonit06@gmail.com>
2024-04-09 04:22:12 +0200

    fixed Client id error

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const findOrCreate = require("mongoose-findorcreate");
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const findOrCreate = require('mongoose-findorcreate')
 
-const app = express();
+const app = express()
+const port = process.env.PORT ?? 3000
+const url = process.env.environment === 'production' ? process.env.URL : 'http://localhost:' + port
 
-app.use(express.static("public"));
-app.use(express.json());  
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'))
+app.use(express.json())
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(
   session({
@@ -25,23 +27,21 @@ app.use(
       maxAge: 3 * 60 * 60 * 1000, // 3 hours
     },
   })
-);
+)
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 async function connection(username, password) {
-  await mongoose.connect(
-    `mongodb+srv://${username}:${password}@cluster0.whaqhs3.mongodb.net/userDB`,
-    {
-      serverSelectionTimeoutMS: 150000,
-      socketTimeoutMS: 150000,
-    }
-  );
+  await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.whaqhs3.mongodb.net/userDB`, {
+    serverSelectionTimeoutMS: 150000,
+    socketTimeoutMS: 150000,
+  })
+  console.log('db connected')
 }
 
-connection(process.env.USER_MONGODB, process.env.PASSWORD_MONGODB);
+connection(process.env.USER_MONGODB, process.env.PASSWORD_MONGODB)
 
 const userSchema = new mongoose.Schema({
   email: String,
@@ -49,28 +49,29 @@ const userSchema = new mongoose.Schema({
   username: String,
   googleId: String,
   score: Number,
-});
+})
 
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
+userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema)
 
 passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
+  done(null, user._id)
+})
 
 passport.deserializeUser((id, done) => {
   User.findById(id, (err, user) => {
-    done(err, user);
-  });
-});
+    done(err, user)
+  })
+})
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "https://naval-battles.mowael.com/auth/google/game",
+      callbackURL: `${url}/auth/google/game`,
     },
     function (accessToken, refreshToken, profile, cb) {
       User.findOrCreate(
@@ -78,194 +79,167 @@ passport.use(
           googleId: profile.id,
         },
         function (err, user) {
-          return cb(err, user);
+          return cb(err, user)
         }
-      );
+      )
     }
   )
-);
+)
 
-let status;
-let alreadyLogged;
-let sameUser;
+let status
+let alreadyLogged
+let sameUser
 
-app.get("/", function (req, res) {
-  res.render("home", { invalidCredentails: status });
-  status = "";
-});
+app.get('/', function (req, res) {
+  res.render('home', { invalidCredentails: status })
+  status = ''
+})
 
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile"] })
-);
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }))
 
-app.get(
-  "/auth/google/game",
-  passport.authenticate("google", { failureRedirect: "/" }),
-  function (req, res) {
-    res.redirect("/game");
-  }
-);
+app.get('/auth/google/game', passport.authenticate('google', { failureRedirect: '/' }), function (req, res) {
+  res.redirect('/game')
+})
 
-app.get("/register", function (erq, res) {
-  res.render("register", { email: alreadyLogged, userTaken: sameUser });
-  alreadyLogged = "";
-  sameUser = "";
-});
+app.get('/register', function (erq, res) {
+  res.render('register', { email: alreadyLogged, userTaken: sameUser })
+  alreadyLogged = ''
+  sameUser = ''
+})
 
-app.post("/register", function (req, res) {
-  User.findOne(
-    { $or: [{ email: req.body.email }, { username: req.body.username }] },
-    function (err, existingUser) {
-      if (existingUser) {
-        if (existingUser.email === req.body.email) {
-          alreadyLogged = "You'd already logged with this email";
-        } else {
-          sameUser = "This username is already taken";
-        }
-        res.redirect("/register");
+app.post('/register', function (req, res) {
+  User.findOne({ $or: [{ email: req.body.email }, { username: req.body.username }] }, function (err, existingUser) {
+    if (existingUser) {
+      if (existingUser.email === req.body.email) {
+        alreadyLogged = "You'd already logged with this email"
       } else {
-        User.register(
-          {
-            username: req.body.username,
-            email: req.body.email,
-            score: 0,
-          },
-          req.body.password,
-          function (err, user) {
-            if (err) {
-              console.log(err);
-              res.redirect("/register");
-            } else {
-              req.logIn(user, function (err) {
-                if (err) {
-                  console.error(err);
-                  return res
-                    .status(500)
-                    .send("An error occurred while trying to log in.");
-                }
-                req.session.username = req.body.username;
-                return res.redirect("/game");
-              });
-            }
-          }
-        );
+        sameUser = 'This username is already taken'
       }
-    }
-  );
-});
-
-app.post("/login", function (req, res) {
-  req.logout(function (err) {
-    if (err) {
-      console.log(err);
-      return res.status(500).send("An error occurred while trying to log in.");
+      res.redirect('/register')
     } else {
-      User.authenticate()(
-        req.body.username,
+      User.register(
+        {
+          username: req.body.username,
+          email: req.body.email,
+          score: 0,
+        },
         req.body.password,
         function (err, user) {
           if (err) {
-            console.error(err);
-            return res
-              .status(500)
-              .send("An error occurred while trying to log in.");
+            console.log(err)
+            res.redirect('/register')
+          } else {
+            req.logIn(user, function (err) {
+              if (err) {
+                console.error(err)
+                return res.status(500).send('An error occurred while trying to log in.')
+              }
+              req.session.username = req.body.username
+              return res.redirect('/game')
+            })
           }
-          if (!user) {
-            status = "invalid username or password";
-            return res.redirect("/");
-          }
-          req.logIn(user, function (err) {
-            if (err) {
-              console.error(err);
-              return res
-                .status(500)
-                .send("An error occurred while trying to log in.");
-            }
-            req.session.username = req.body.username;
-            return res.redirect("/game");
-          });
         }
-      );
+      )
     }
-  });
-});
+  })
+})
 
-app.get("/game", function (req, res) {
+app.post('/login', function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      console.log(err)
+      return res.status(500).send('An error occurred while trying to log in.')
+    } else {
+      User.authenticate()(req.body.username, req.body.password, function (err, user) {
+        if (err) {
+          console.error(err)
+          return res.status(500).send('An error occurred while trying to log in.')
+        }
+        if (!user) {
+          status = 'invalid username or password'
+          return res.redirect('/')
+        }
+        req.logIn(user, function (err) {
+          if (err) {
+            console.error(err)
+            return res.status(500).send('An error occurred while trying to log in.')
+          }
+          req.session.username = req.body.username
+          return res.redirect('/game')
+        })
+      })
+    }
+  })
+})
+
+app.get('/game', function (req, res) {
   if (req.isAuthenticated()) {
     if (!req.user.username) {
-      req.session.data = { userId: req.user.googleId };
-      res.redirect("/create-user");
+      req.session.data = { userId: req.user.googleId }
+      res.redirect('/create-user')
     } else {
-      res.render("game", {
+      res.render('game', {
         username: req.user.username,
         score: req.user.score,
-      });
+      })
     }
   } else {
-    res.redirect("/");
+    res.redirect('/')
   }
-});
+})
 
-app.get("/game/users", async (req, res) => {
+app.get('/game/users', async (req, res) => {
+  console.log('users')
   if (req.isAuthenticated()) {
     const users = await User.find(
       { $or: [{ score: { $gt: 0 } }, { username: req.session.username }] },
       { _id: 0, username: 1, score: 1 }
-    );
-    res.json(users);
+    )
+    res.json(users)
   } else {
-    res.send("you are not authenticated");
+    res.send('you are not authenticated')
   }
-});
+})
 
-app.post("/game/users/:username", async function (req, res) {
+app.post('/game/users/:username', async function (req, res) {
   if (req.isAuthenticated()) {
     try {
-      await User.findOneAndUpdate(
-        { username: req.params.username },
-        { score: req.body.score },
-        { new: true }
-      );
-      res.json("User score updated successfully");
+      await User.findOneAndUpdate({ username: req.params.username }, { score: req.body.score }, { new: true })
+      res.json('User score updated successfully')
     } catch (error) {
-      res.status(500).json({ error: "internal server error" });
+      res.status(500).json({ error: 'internal server error' })
     }
   }
-});
+})
 
-app.get("/create-user", function (req, res) {
+app.get('/create-user', function (req, res) {
   if (req.isAuthenticated()) {
-    res.render("create-user", { userTaken: sameUser });
-    sameUser = "";
+    res.render('create-user', { userTaken: sameUser })
+    sameUser = ''
   } else {
-    res.redirect("/");
+    res.redirect('/')
   }
-});
+})
 
-app.post("/create-user", function (req, res) {
+app.post('/create-user', function (req, res) {
   User.findOne({ username: req.body.username }, function (err, existingUser) {
     if (existingUser) {
-      sameUser = "This username is already taken";
-      res.redirect("/create-user");
+      sameUser = 'This username is already taken'
+      res.redirect('/create-user')
     } else {
-      const userId = req.session.data.userId;
-      User.updateOne(
-        { googleId: userId },
-        { username: req.body.username, score: 0 },
-        function (err) {
-          if (err) {
-            console.log(err);
-            res.redirect("/");
-          } else {
-            res.redirect("/game");
-          }
+      const userId = req.session.data.userId
+      User.updateOne({ googleId: userId }, { username: req.body.username, score: 0 }, function (err) {
+        if (err) {
+          console.log(err)
+          res.redirect('/')
+        } else {
+          res.redirect('/game')
         }
-      );
+      })
     }
-  });
-});
+  })
+})
 
-app.listen(4000, function () {
-  console.log("server started on port 4000");
-});
+app.listen(port, function () {
+  console.log('server started on port ' + port)
+})
--- public/main.js
+++ public/main.js
@@ -1,57 +1,54 @@
-let players = document.querySelector("ul.players");
-const usernameForMain = document.getElementById("username");
-const userScore = document.getElementById("score");
-const loader = document.querySelector(".lds-ripple");
+let players = document.querySelector('ul.players')
+const usernameForMain = document.getElementById('username')
+const userScore = document.getElementById('score')
+const loader = document.querySelector('.lds-ripple')
 
 async function getUsers() {
   try {
-    const response = await fetch("https://naval-battles.mowael.com/game/users");
-    loader.style.display = "none";
-    const usersArray = await response.json();
+    const response = await fetch('https://naval-battles.mowael.com/game/users')
+    loader.style.display = 'none'
+    const usersArray = await response.json()
     for (let i = 0; i < usersArray.length; i++) {
       for (let j = i; j < usersArray.length; j++) {
         if (usersArray[i].score < usersArray[j].score) {
-          [usersArray[i], usersArray[j]] = [usersArray[j], usersArray[i]];
+          ;[usersArray[i], usersArray[j]] = [usersArray[j], usersArray[i]]
         }
       }
     }
     usersArray.forEach((user) => {
-      let li = document.createElement("li");
-      let userName = document.createElement("span");
-      let userScore = document.createElement("span");
-      userName.textContent = user.username;
-      userScore.textContent = user.score;
+      let li = document.createElement('li')
+      let userName = document.createElement('span')
+      let userScore = document.createElement('span')
+      userName.textContent = user.username
+      userScore.textContent = user.score
       if (user.username === usernameForMain.textContent) {
-        userName.className = "active";
-        userScore.className = "active";
+        userName.className = 'active'
+        userScore.className = 'active'
       }
-      li.append(userName, userScore);
-      players.append(li);
-    });
+      li.append(userName, userScore)
+      players.append(li)
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
 
-getUsers();
+getUsers()
 
 export async function setScore() {
   try {
-    const response = await fetch(
-      `https://naval-battles.mowael.com/game/users/${usernameForMain.textContent}`,
-      {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          score: +userScore.textContent + 1,
-        }),
-      }
-    );
-    const data = await response.json(); // will wait a response from server
-    console.log(data);
+    const response = await fetch(`https://naval-battles.mowael.com/game/users/${usernameForMain.textContent}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        score: +userScore.textContent + 1,
+      }),
+    })
+    const data = await response.json() // will wait a response from server
+    console.log(data)
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
